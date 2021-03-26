687720cbaffd1f6c261e842c6975d079d7749125
inv2004 <inv2004@gmail.com>
2021-03-26 13:08:41 +0300

    rename fields in bench

--- tests/bench1.nim
+++ tests/bench1.nim
@@ -18,16 +18,16 @@ benchmark cfg:
     doAssert "e" == f.t35
     doAssert "TTTTTTT6" == f.t49
     doAssert "20140709-14:43:12.934" == f.t52
-    doAssert "6236.83333333" == f.t155
+    doAssert "6236.83333333" == f.t5555
     doAssert "872" == f.t56
     doAssert "ARCA" == f.t57
-    doAssert 'Y' == f.t77
+    doAssert 'Y' == f.t7777
     doAssert 0 == f.t98
     doAssert 60 == f.t108
     f.t108
 
-  # proc benchFix1Field() {.measure.} =
-  #   blackBox fix1()
+  proc benchFix1Field() {.measure.} =
+    blackBox fix1()
 
   proc fix2(): int =
     let f = parseFix2(s)
@@ -38,16 +38,16 @@ benchmark cfg:
     doAssert "e" == f.t35
     doAssert "TTTTTTT6" == f.t49
     doAssert "20140709-14:43:12.934" == f.t52
-    doAssert "6236.83333333" == f.t155
+    doAssert "6236.83333333" == f.t5555
     doAssert "872" == f.t56
     doAssert "ARCA" == f.t57
-    doAssert 'Y' == f.t77
+    doAssert 'Y' == f.t7777
     doAssert 0 == f.t98
     doAssert 60 == f.t108
     f.t108
 
-  # proc benchFix2Field() {.measure.} =
-  #   blackBox fix2()
+  proc benchFix2Field() {.measure.} =
+    blackBox fix2()
 
   proc fix3(): int =
     let f = parseFix3(s)
@@ -58,16 +58,16 @@ benchmark cfg:
     doAssert "e" == f.t35
     doAssert "TTTTTTT6" == f.t49
     doAssert "20140709-14:43:12.934" == f.t52
-    doAssert "6236.83333333" == f.t155
+    doAssert "6236.83333333" == f.t5555
     doAssert "872" == f.t56
     doAssert "ARCA" == f.t57
-    doAssert 'Y' == f.t77
+    doAssert 'Y' == f.t7777
     doAssert 0 == f.t98
     doAssert 60 == f.t108
     f.t108  
 
-  # proc benchFix3Field() {.measure.} =
-  #   blackBox fix3()
+  proc benchFix3Field() {.measure.} =
+    blackBox fix3()
 
   proc fix4(): int =
     let f = parseFix4(s)
@@ -78,16 +78,16 @@ benchmark cfg:
     doAssert "e" == f.t35
     doAssert "TTTTTTT6" == f.t49
     doAssert "20140709-14:43:12.934" == f.t52
-    doAssert "6236.83333333" == f.t155
+    doAssert "6236.83333333" == f.t5555
     doAssert "872" == f.t56
     doAssert "ARCA" == f.t57
-    doAssert 'Y' == f.t77
+    doAssert 'Y' == f.t7777
     doAssert 0 == f.t98
     doAssert 60 == f.t108
     f.t108
 
-  # proc benchFix4Field() {.measure.} =
-  #   blackBox fix4()
+  proc benchFix4Field() {.measure.} =
+    blackBox fix4()
 
   proc fix44(): int =
     let f = parseFix44(s)
@@ -100,10 +100,10 @@ benchmark cfg:
     doAssert mtELow == f.msgType
     doAssert "TTTTTTT6" == f.senderCompID
     doAssert "20140709-14:43:12.934" == f.sendingTime
-    doAssert "6236.83333333" == f.elowSettlCurrFxRate
+    doAssert "6236.83333333" == f.elowUnknown1
     doAssert "872" == f.targetCompID
     doAssert "ARCA" == f.elowTargetSubID
-    doAssert 'Y' == f.elowPositionEffect
+    doAssert 'Y' == f.elowUnknown2
     doAssert 0 == f.elowEncryptMethod
     doAssert 60 == f.elowHeartBtInt
     f.elowHeartBtInt
