3c9695994ed84ad28dd8171c7bc262502b643c27
inv2004 <inv2004@gmail.com>
2021-04-04 01:19:40 +0300

    memcmp + memchr version

--- src/newfix/streamfix.nim
+++ src/newfix/streamfix.nim
@@ -1,4 +1,3 @@
-import strutils
 import criterion
 
 type
@@ -6,9 +5,20 @@ type
     pos: int
     msg: string
 
+type
+  StreamFix2 = object
+    pos: pointer
+    msg: string
+    last: pointer
+
 proc initFix(msg: string): StreamFix =
   result.pos = 0
   result.msg = msg
+
+proc initFix2(msg: string): StreamFix2 =
+  result.msg = msg
+  result.pos = result.msg[0].unsafeAddr
+  result.last = result.msg[result.msg.len-1].unsafeAddr
 
 proc tag1(f: var StreamFix, tag: int): string =
   let l = f.msg.len
@@ -29,6 +39,27 @@ proc tag1(f: var StreamFix, tag: int): string =
         inc f.pos
       inc f.pos
 
+proc c_memcmp*(a, b: pointer, size: csize_t): cint {.
+              importc: "memcmp", header: "<string.h>", noSideEffect.}
+
+proc c_memchr(cstr: pointer, c: char, n: csize_t): pointer {.
+              importc: "memchr", header: "<string.h>".}
+
+proc tag2(f: var StreamFix2, tag: string): string =
+  let tLen = cast[csize_t](tag.len)
+  let tAddr = tag[0].unsafeAddr
+  while f.pos < f.last:
+    if 0 == c_memcmp(f.pos, tAddr, tLen):
+      f.pos = cast[pointer](cast[uint](f.pos) + tLen)
+      let nextAddr = c_memchr(f.pos, '\x01', cast[csize_t](1000))
+      let len = cast[ByteAddress](nextAddr) - cast[ByteAddress](f.pos)
+      result = newString(len)
+      copyMem(result[0].addr, f.pos, len)
+      return
+    else:
+      f.pos = c_memchr(f.pos, '\x01', cast[csize_t](1000))
+      f.pos = cast[pointer](cast[uint](f.pos) + 1u)
+
 var cfg = newDefaultConfig()
 
 benchmark cfg:
@@ -39,19 +70,22 @@ benchmark cfg:
     for i in 1..20:
       result += f.tag1(190).len
 
-  # proc fix4(): int =
-  #   var pos = 0
-  #   for i in 1..20:
-  #     result += tag4(s4, pos, 190).len
+  proc fix2(): int =
+    var f = initFix2(s4)
+    for i in 1..20:
+      result += f.tag2("190=").len
 
   proc benchFix1() {.measure.} =
     blackBox fix1()
 
-# proc main() =
-#   let s = readLines("tests/test1.fix", 5)
-#   var f1 = initFix(s[4])
-#   var f2 = initFix(s[4])
-#   for i in 1..20:
-#     echo f1.tag3(190), " == ", f2.tag4(190)
+  proc benchFix2() {.measure.} =
+    blackBox fix2()
+
+proc main() =
+  let s = readLines("tests/test1.fix", 5)
+  var f1 = initFix(s[4])
+  var f2 = initFix2(s[4])
+  for i in 1..20:
+    echo f1.tag1(190), " == ", f2.tag2("190=")
 
 # main()
