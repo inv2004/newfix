c2d9cfa648d05483b703838fbb227592783ef90c
inv2004 <inv2004@gmail.com>
2021-04-04 23:42:53 +0300

    f.c two benches

--- src/newfix/f.c
+++ src/newfix/f.c
@@ -25,29 +25,7 @@ FS ffree(FS fs) {
   free(fs);
 }
 
-char* ftag(FS fs, char* tag) {
-  char* result;
-  int tlen = strlen(tag);
-
-  while(fs->pos < fs->last) {
-    if(0 == memcmp(fs->pos, tag, tlen)) {
-      fs->pos += tlen;
-      char* next = memchr(fs->pos, '\x01', 1000);
-      int len = next - fs->pos;
-      result = malloc(len + 1);
-      memcpy(result, fs->pos, len);
-      result[len] = '\0';
-      fs->pos = next;
-      return result;
-    } else {
-      fs->pos = 1 + memchr(fs->pos, '\x01', 1000);
-    }
-  }
-  result = malloc(1);
-  result[0] = '\0';
-  return result;
-}
-char* ftag2(FS fs, int tag) {
+char* ftag1(FS fs, int tag) {
   char* result;
   int t;
   while(fs->pos < fs->last) {
@@ -79,13 +57,43 @@ char* ftag2(FS fs, int tag) {
   return result;
 }
 
-int bench(char * buf) {
-  FS f = finit(buf);
+char* ftag3(FS fs, char* tag) {
+  char* result;
+  int tlen = strlen(tag);
+
+  while(fs->pos < fs->last) {
+    if(0 == memcmp(fs->pos, tag, tlen)) {
+      fs->pos += tlen;
+      char* next = memchr(fs->pos, '\x01', 1000);
+      int len = next - fs->pos;
+      result = malloc(len + 1);
+      memcpy(result, fs->pos, len);
+      result[len] = '\0';
+      fs->pos = next;
+      return result;
+    } else {
+      fs->pos = 1 + memchr(fs->pos, '\x01', 1000);
+    }
+  }
+  result = malloc(1);
+  result[0] = '\0';
+  return result;
+}
+
+int bench1(char * buf) {
   int result;
+  FS f = finit(buf);
   for(int i = 0; i < 20; i++) {
-    // printf("%s\n", ftag2(f, 190));
-    // result += ftag(f, "190=")[0];
-    result += ftag2(f, 190)[0];
+    result += ftag1(f, 190)[0];
+  }
+  return result;
+}
+
+int bench3(char * buf) {
+  int result;
+  FS f = finit(buf);
+  for(int i = 0; i < 20; i++) {
+    result += ftag3(f, "190=")[0];
   }
   return result;
 }
@@ -102,18 +110,22 @@ int main() {
 
   // struct timespec start, end;
   // clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+  int result = 0;
   clock_t start = clock();
-
-  int result;
   for(int i = 0; i < 1000; i++) {
-    result += bench(line);
+    result += bench1(line);
   }
-  // clock_gettime(CLOCK_MONOTONIC_RAW, &end);
   clock_t stop = clock();
   double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+  printf("%d: %f\n", result, elapsed / 1000.0);
 
-  // unsigned long delta = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000; 
-  // delta /= 200;
+  result = 0;
+  start = clock();
+  for(int i = 0; i < 1000; i++) {
+    result += bench3(line);
+  }
+  stop = clock();
+  elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
   printf("%d: %f\n", result, elapsed / 1000.0);
 
   exit(EXIT_SUCCESS);
